5fe4bfa91e692a2eee2cc17ff0baac743d99e3be
Jae-Joon Lee <lee.j.joon@gmail.com>
2009-12-13 15:55:37 -0500

    gh-pages: update allsky

--- plot_directive/allsky/allsky.py
+++ plot_directive/allsky/allsky.py
@@ -18,27 +18,27 @@ import matplotlib.patheffects
 
 def allsky_header():
     # header retrieved from "lambda_mollweide_halpha_fwhm06_0512.fits"
-    header = """XTENSION= 'IMAGE   '           / IMAGE extension               
-BITPIX  =                  -32 / Number of bits per data pixel                  
-NAXIS   =                    2 / Number of data axes                            
-NAXIS1  =                 4096 /                                                
-NAXIS2  =                 2048 /                                                
-PCOUNT  =                    0 / No Group Parameters                            
-GCOUNT  =                    1 / One Data Group                                 
-EXTNAME = 'TEMPERATURE'                                                         
-CTYPE1  = 'GLON-MOL'           / Coordinate Type                                
-CTYPE2  = 'GLAT-MOL'           / Coordinate Type                                
-EQUINOX =              2000.00 / Equinox of Ref. Coord.                         
-CDELT1  =     -0.0791293637247 / Degrees / Pixel                                
-CDELT2  =      0.0791293637247 / Degrees / Pixel                                
-CROTA2  =              0.00000 / Rotation Angle (Degrees)                       
-CRPIX1  =              2048.50 / Reference Pixel in X                           
-CRPIX2  =              1024.50 / Reference Pixel in Y                           
-CRVAL1  =        0.00000000000 / Galactic longitude of reference pixel          
-CRVAL2  =        0.00000000000 / Galactic latitude of reference pixel           
-LONPOLE =        0.00000000000 / Native longitude of Galactic pole              
-LATPOLE =        0.00000000000 / Galactic latitude of native pole               
-PV2_1   =        0.00000000000 /Projection parameter 1                          
+    header = """XTENSION= 'IMAGE   '           / IMAGE extension
+BITPIX  =                  -32 / Number of bits per data pixel
+NAXIS   =                    2 / Number of data axes
+NAXIS1  =                 4096 /
+NAXIS2  =                 2048 /
+PCOUNT  =                    0 / No Group Parameters
+GCOUNT  =                    1 / One Data Group
+EXTNAME = 'TEMPERATURE'
+CTYPE1  = 'GLON-MOL'           / Coordinate Type
+CTYPE2  = 'GLAT-MOL'           / Coordinate Type
+EQUINOX =              2000.00 / Equinox of Ref. Coord.
+CDELT1  =     -0.0791293637247 / Degrees / Pixel
+CDELT2  =      0.0791293637247 / Degrees / Pixel
+CROTA2  =              0.00000 / Rotation Angle (Degrees)
+CRPIX1  =              2048.50 / Reference Pixel in X
+CRPIX2  =              1024.50 / Reference Pixel in Y
+CRVAL1  =        0.00000000000 / Galactic longitude of reference pixel
+CRVAL2  =        0.00000000000 / Galactic latitude of reference pixel
+LONPOLE =        0.00000000000 / Native longitude of Galactic pole
+LATPOLE =        0.00000000000 / Galactic latitude of native pole
+PV2_1   =        0.00000000000 /Projection parameter 1
 HISTORY PUTAST: Jun 17 14:36:35 2009 World Coordinate System parameters written
 """
     cards = pyfits.CardList()
@@ -62,7 +62,7 @@ if 1:
     else:
         data = None
         header = allsky_header()
-        
+
 
     fig = plt.figure(1, figsize=(8, 5))
     fig.clf()
@@ -96,28 +96,29 @@ if 1:
     #for an in ["bottom", "top"]:
     ax.axis["bottom", "top"].set_visible(False)
 
+    ax.axis["right"].set_axis_direction("left")
+
     axis = ax.axis["left"]
     axis.set_axis_direction("right")
     axis.major_ticklabels._text_follow_ref_angle=False
-    axis.major_ticklabels.set_rotation(0)
-    axis.major_ticklabels.set(va="center", ha="center")
-    axis.major_ticklabels.set_pad(0) #(va="center", ha="center")
+    axis.major_ticklabels.set(rotation=0,
+                              va="center", ha="center",
+                              pad=0)
     axis.label._text_follow_ref_angle=False
     axis.label.set(rotation=0, va="center", ha="right")
 
-    axis = ax.axis["right"]
-    axis.set_axis_direction("left")
 
     ax.axis["b=0"] = grid_helper.new_floating_axis(nth_coord=1, value=0,
                                                    axes=ax,
                                                    axis_direction='bottom')
+
     axis = ax.axis["b=0"]
+    axis.get_helper().set_extremes(-180, 180)
 
     ef = matplotlib.patheffects.withStroke(foreground="w", linewidth=3)
     axis.major_ticklabels.set_path_effects([ef])
     axis.label.set_path_effects([ef])
 
     ax.grid()
-
     plt.show()
 
