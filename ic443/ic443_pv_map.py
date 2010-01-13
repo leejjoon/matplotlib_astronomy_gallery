9870346709d173b41230281433a4710ddb07d035
Jae-Joon Lee <lee.j.joon@gmail.com>
2010-01-13 17:26:22 -0500

    update plotting code in ic443 to have setup_axes function

--- ic443/ic443_pv_map.py
+++ ic443/ic443_pv_map.py
@@ -16,64 +16,76 @@ class Velo(object):
         return (p + 1 - self.crpix)*self.cdelt + self.crval
 
 
+def setup_axes(fig, header):
+    from mpl_toolkits.axes_grid import make_axes_locatable
+
+    ax0 = pywcsgrid2.subplot(111, wcs=header)
+    divider = make_axes_locatable(ax0)
+
+    ax_v = divider.new_vertical(1.5, pad=0.1, sharex=ax0,
+                                axes_class=axislines.Axes)
+    fig.add_axes(ax_v)
+
+    ax_h = divider.new_horizontal(1.5, pad=0.1, sharey=ax0,
+                                  axes_class=axislines.Axes)
+
+    fig.add_axes(ax_h)
+
+
+    new_axis = ax0.get_grid_helper().new_fixed_axis
+    ax_h.axis["left"] = new_axis("left", axes=ax_h)
+    ax_h.axis["right"] = new_axis("right", axes=ax_h)
+    ax_h.axis["left"].toggle(ticklabels=False)
+    ax_h.axis["right"].toggle(ticklabels=False)
+
+    ax_v.axis["top"] = new_axis("top", axes=ax_v)
+    ax_v.axis["bottom"] = new_axis("bottom", axes=ax_v)
+    ax_v.axis["top"].toggle(ticklabels=False)
+    ax_v.axis["bottom"].toggle(ticklabels=False)
+
+
+    return ax0, ax_v, ax_h
+
+
 fits_cube = pyfits.open("ic443_co.ambient.fits")
 header = fits_cube[0].header
 
 vel = Velo(header)
 
-from mpl_toolkits.axes_grid import make_axes_locatable
+
 import matplotlib.colors as mcolors
 norm = mcolors.Normalize()
 cmap = plt.cm.gray_r
 
-fig = plt.figure(1, figsize=(6, 6))
-
-ax0 = pywcsgrid2.subplot(111, wcs=header)
-divider = make_axes_locatable(ax0)
-
 fits_cube[0].data = fits_cube[0].data**2
 
 vv0  = fits_cube[0].data.max(axis=0) #.sum(axis=1)
+vv1  = fits_cube[0].data.max(axis=1)
+vv2  = fits_cube[0].data.max(axis=2).transpose()
+
+
+fig = plt.figure(1, figsize=(6, 6))
+ax0, ax_v, ax_h = setup_axes(fig, header)
 
 im0 = ax0.imshow(vv0, origin="lower", interpolation="nearest",
                  norm=norm, cmap=cmap)
 
-ax_v = divider.new_vertical(1.5, pad=0.1, sharex=ax0,
-                            axes_class=axislines.Axes)
-fig.add_axes(ax_v)
-vv1  = fits_cube[0].data.max(axis=1)
 ax_v.imshow(vv1, origin="lower", aspect="auto",
             interpolation="nearest", norm=norm, cmap=cmap)
 
 
-ax_h = divider.new_horizontal(1.5, pad=0.1, sharey=ax0,
-                              axes_class=axislines.Axes)
-
-fig.add_axes(ax_h)
-vv2  = fits_cube[0].data.max(axis=2).transpose()
 ax_h.imshow(vv2, origin="lower", aspect="auto",
             interpolation="nearest", norm=norm,
             cmap=cmap)
+
+ax_h.axis["bottom"].label.set_text(r"$v_{\mathrm{LSR}}$ [km s$^{-1}$]")
+ax_v.axis["left"].label.set_text(r"$v_{\mathrm{LSR}}$ [km s$^{-1}$]")
 
 
 norm.vmin=0.1
 norm.vmax=5**2
 for ax in [ax0, ax_h, ax_v]:
     ax.images[0].changed()
-
-new_axis = ax0.get_grid_helper().new_fixed_axis
-ax_h.axis["left"] = new_axis("left", axes=ax_h)
-ax_h.axis["right"] = new_axis("right", axes=ax_h)
-ax_h.axis["left"].toggle(ticklabels=False)
-ax_h.axis["right"].toggle(ticklabels=False)
-
-ax_v.axis["top"] = new_axis("top", axes=ax_v)
-ax_v.axis["bottom"] = new_axis("bottom", axes=ax_v)
-ax_v.axis["top"].toggle(ticklabels=False)
-ax_v.axis["bottom"].toggle(ticklabels=False)
-
-ax_h.axis["bottom"].label.set_text(r"$v_{\mathrm{LSR}}$ [km s$^{-1}$]")
-ax_v.axis["left"].label.set_text(r"$v_{\mathrm{LSR}}$ [km s$^{-1}$]")
 
 v = [-10, -5, 0, -5]
 vl = [("$%d$" % v1) for v1 in v]
@@ -97,7 +109,4 @@ for t in gh.get_tick_iterator(1, "left"):
 
 
 plt.show()
-
-if 0:
-    plt.savefig("co_peakT_pv_map.eps", dpi=70, bbox_inches="tight")
 
