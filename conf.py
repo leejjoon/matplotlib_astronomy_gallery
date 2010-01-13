31f647e3602b8923883565ae100d73742e219abb
Jae-Joon Lee <lee.j.joon@gmail.com>
2010-01-13 17:25:47 -0500

    modify plot_directive.py to use local rc settings

--- conf.py
+++ conf.py
@@ -16,15 +16,15 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.append(os.path.abspath('./sphinxext'))
+sys.path.insert(0, os.path.abspath('./sphinxext'))
 
 # -- General configuration -----------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.todo',
-              'matplotlib.sphinxext.plot_directive',
               'matplotlib.sphinxext.only_directives',
+              'plot_directive_jj',
               'github_sphinx',
               ]
 
