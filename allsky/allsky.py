1aa43a8a5abda257bcfe076d89c7b7802c55c655
Jae-Joon Lee <lee.j.joon@gmail.com>
2011-03-21 23:26:27 +0900

    modify allsky.py to use new allsky_axes module

--- allsky/allsky.py
+++ allsky/allsky.py
@@ -1,126 +1,41 @@
 import matplotlib.pyplot as plt
 import pyfits
 
-from mpl_toolkits.axisartist.grid_finder import FixedLocator
-from mpl_toolkits.axisartist.floating_axes import floatingaxes_class_factory
+import matplotlib.patheffects
 
-from mpl_toolkits.axisartist.angle_helper import LocatorDMS, LocatorHMS, \
-     FormatterHMS, FormatterDMS
-
-from pywcsgrid2.axes_wcs import GridHelperWcsFloating, AxesWcs
-
-import matplotlib.axes as maxes
-
-use_path_effects = True
-try:
-    import matplotlib.patheffects
-except ImportError:
-    use_path_effects = False
-
-
-
-def allsky_header():
-    # header retrieved from "lambda_mollweide_halpha_fwhm06_0512.fits"
-    header = """XTENSION= 'IMAGE   '           / IMAGE extension
-BITPIX  =                  -32 / Number of bits per data pixel
-NAXIS   =                    2 / Number of data axes
-NAXIS1  =                 4096 /
-NAXIS2  =                 2048 /
-PCOUNT  =                    0 / No Group Parameters
-GCOUNT  =                    1 / One Data Group
-EXTNAME = 'TEMPERATURE'
-CTYPE1  = 'GLON-MOL'           / Coordinate Type
-CTYPE2  = 'GLAT-MOL'           / Coordinate Type
-EQUINOX =              2000.00 / Equinox of Ref. Coord.
-CDELT1  =     -0.0791293637247 / Degrees / Pixel
-CDELT2  =      0.0791293637247 / Degrees / Pixel
-CROTA2  =              0.00000 / Rotation Angle (Degrees)
-CRPIX1  =              2048.50 / Reference Pixel in X
-CRPIX2  =              1024.50 / Reference Pixel in Y
-CRVAL1  =        0.00000000000 / Galactic longitude of reference pixel
-CRVAL2  =        0.00000000000 / Galactic latitude of reference pixel
-HISTORY PUTAST: Jun 17 14:36:35 2009 World Coordinate System parameters written
-"""
-    cards = pyfits.CardList()
-    for l in header.split("\n"):
-        card = pyfits.Card()
-        card.fromstring(l.strip())
-        cards.append(card)
-    h = pyfits.Header(cards)
-    return h
 
 
 
 if 1:
-    import os.path
 
     fits_name = "lambda_mollweide_halpha_fwhm06_0512.fits"
-    if os.path.exists(fits_name):
-        f = pyfits.open(fits_name)
-        data = f[1].data
-        header = f[1].header
-    else:
-        data = None
-    header = allsky_header()
+    f = pyfits.open(fits_name)
+    data = f[1].data
+    header = f[1].header
 
+    # used fixed header
+    del header["LONPOLE"]
+    del header["LATPOLE"]
 
     fig = plt.figure(1, figsize=(8, 5))
     fig.clf()
 
-    FloatingAxes = floatingaxes_class_factory(AxesWcs)
-    FloatingSubplot = maxes.subplot_class_factory(FloatingAxes)
+    from pywcsgrid2.allsky_axes import make_allsky_axes_from_header
 
-
-    grid_locator1 = FixedLocator([-180, -120, -60, 0, 60, 120, 180])
-    grid_locator2 = FixedLocator([-90, -60, -30, 0, 30, 60, 90])
-    tick_formatter1=FormatterHMS()
-    tick_formatter2=FormatterDMS()
-
-    grid_helper = GridHelperWcsFloating(wcs=header,
-                                        extremes=(180, -180, -90, 90),
-                                        grid_locator1=grid_locator1,
-                                        grid_locator2=grid_locator2,
-                                        tick_formatter1=tick_formatter1,
-                                        tick_formatter2=tick_formatter2,
-                                        )
-
-    #grid_helper.set_lon_ref(-180)
-
-    ax = FloatingSubplot(fig, 111, grid_helper=grid_helper)
-    ax.set_autoscale_on(False)
-    fig.add_subplot(ax)
+    ax = make_allsky_axes_from_header(fig, rect=111, header=header,
+                                      lon_center=0.)
 
     if data is not None:
         im = ax.imshow(data, origin="lower", cmap=plt.cm.gray_r)
         im.set_clip_path(ax.patch)
         im.set_clim(0, 30)
 
-    ax.axis["bottom", "top"].set_visible(False)
-
-    axis = ax.axis["left"]
-    axis.set_axis_direction("left")
-    axis.major_ticklabels._text_follow_ref_angle=False
-    axis.major_ticklabels.set(rotation=0,
-                              va="center", ha="center",
-                              pad=0)
-    axis.label._text_follow_ref_angle=False
-    axis.label.set(rotation=0, va="center", ha="right")
-
-
-    ax.axis["b=0"] = grid_helper.new_floating_axis(nth_coord=1, value=0,
-                                                   axes=ax,
-                                                   axis_direction='bottom')
-
-    axis = ax.axis["b=0"]
-    axis.get_helper().set_extremes(180, -180)
-    axis.set_ticklabel_direction("-")
-    axis.set_axislabel_direction("-")
-
-    if use_path_effects:
-        ef = matplotlib.patheffects.withStroke(foreground="w", linewidth=3)
-        axis.major_ticklabels.set_path_effects([ef])
-        axis.label.set_path_effects([ef])
-
+    ef = matplotlib.patheffects.withStroke(foreground="w", linewidth=3)
+    axis = ax.axis["lat=0"]
+    axis.major_ticklabels.set_path_effects([ef])
+    axis.label.set_path_effects([ef])
+    axis.set_zorder(5)
+        
     ax.grid()
     plt.show()
 
